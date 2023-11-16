da88ee037cdb4ab6d8d7d7567749b21e3bd0a710
bergina jonathan <ext.jonathan.bergina@sncf.fr>
2023-11-16 14:19:42 +0100

    liaison form ajax

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,39 +8,68 @@ use App\Entity\Contracts;
 use App\Form\ContactLinkingFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(Request $request,EntityManagerInterface  $entityManager): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
 
-       
+
         $ListeEmployee = $entityManager->getRepository(Employees::class)->findAll();
         $ListeContracts = $entityManager->getRepository(Contracts::class)->findAll();
 
         // var_dump($ListeContracts);
-
-        $controle = new Controle();
-        $form = $this->createForm(ContactLinkingFormType::class,$controle);
-        $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($controle);
-            $entityManager->flush();
-            $this->addFlash('success', 'Votre liaison a bien été enregistrée');
-            return $this->redirectToRoute('app_home');
-        }
 
 
         // Le bouton dans votre template appelle getData lorsque cliqué
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'ListeContracts' => $ListeContracts,
-            'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/liaison', name: 'app_liaison')]
+    public function liaison(Request $request, EntityManagerInterface $entityManager): Response
+    {
+
+        $controle = new Controle();
+        $form = $this->createForm(ContactLinkingFormType::class, $controle, [
+            'action' => $this->generateUrl('app_liaison_create'),
+            'method' => 'POST',
+        ]);
+
+        $controls = $entityManager->getRepository(Controle::class)->findBy([], ['id' => 'DESC']);
+        return $this->render('home/liaison.html.twig', [
+            'controller_name' => 'HomeController',
+            'form' => $form->createView(),
+            'controls' => $controls,
+        ]);
+    }
+
+    #[Route('/liaison/create', name: 'app_liaison_create', methods: ['POST'], condition: "request.isXmlHttpRequest()")]
+    public function liaisonCreate(Request $request, EntityManagerInterface $entityManager,SerializerInterface $serializer): Response
+    {
+        $controle = new Controle();
+        $form = $this->createForm(ContactLinkingFormType::class, $controle, [
+            'action' => $this->generateUrl('app_home'),
+            'method' => 'POST',
+        ]);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($controle);
+            $entityManager->flush();
+            $data = $serializer->serialize($controle, 'json');
+            return new JsonResponse(['success' => true, 'data' => json_decode($data)]);
+        }
+
+        $errors = "";
+            return new JsonResponse(['success' => false, 'errors' => $errors]);
     }
 
     // public function getData(): Response
